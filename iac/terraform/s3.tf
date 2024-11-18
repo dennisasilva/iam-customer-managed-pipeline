0bc9d65304a157b8124516bc7be02f8fa45bc1e8
Dennis Silva <dennisasilva@gmail.com>
2024-11-18 16:57:54 -0300

    Create s3.tf

---
+++ iac/terraform/s3.tf
@@ -0,0 +1,47 @@
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}-cx-mng-artfcs"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "HTTPSOnly",
+        Effect    = "Deny",
+        Principal = "*",
+        Action    = "s3:*",
+        Resource  = [
+          aws_s3_bucket.artifacts.arn,
+          "${aws_s3_bucket.artifacts.arn}/*"
+        ],
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = false
+          }
+        }
+      }
+    ]
+  })
+}
