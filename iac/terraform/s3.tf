bd46139839270696e9a3942b335b6eca0e82b53e
Dennis Silva <dennisasilva@gmail.com>
2024-11-18 17:07:17 -0300

    Update s3.tf

--- iac/terraform/s3.tf
+++ iac/terraform/s3.tf
@@ -9,6 +9,7 @@ resource "aws_s3_bucket" "artifacts" {
     rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
+        bucket_key_enabled = true
       }
     }
   }
